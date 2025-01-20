b4fc234c2861bb3b3016fd3780068e3f7c7a8c44
abhimanyujangid <abhimanyujangid79@gmail.com>
2025-01-20 18:48:31 +0530

    Enhance user tweets retrieval with detailed owner and likes information

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -21,13 +21,87 @@ const createTweet = asyncHandler(async (req, res) => {
     if (!tweet) {
         throw new ApiError(500, "Something went wrong")
     }
-    res.status(201).json(new ApiResponse(201, "Tweet created successfully", tweet))
+   return res.status(201).json(new ApiResponse(201, "Tweet created successfully", tweet))
 })
 
 const getUserTweets = asyncHandler(async (req, res) => {
     // TODO: get user tweets
+    const { userId } = req.params;
+    
+    if (!isValidObjectId(userId)) {
+        throw new ApiError(400, "Invalid user id");
+    }
 
+    const tweet = await Tweet.aggregate([
+        {
+            $match: { owner: mongoose.Types.ObjectId(userId) }
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "owner",
+                foreignField: "_id",
+                as: "OwnerDetails",
+                pipeline:[
+                    {
+                        $project: 
+                        {
+                            username: 1,
+                            avatar: 1
+                        }
+                    }
+                ]
+            }
+        },
+        { 
+            $lookup:{
+                from:"likes",
+                localField: "_id",
+                foreignField: "tweet",
+                as: "likesDetails",
+                pipeline:[
+                    {
+                        $project:{
+                            likedBy: 1
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $addFields:{
+                likesCount: {$size: "$likesDetails"},
+                OwnerDetails: {$arrayElemAt: ["$OwnerDetails", 0]},
+                isLiked:{
+                    $cond:{
+                        if:{$in:[req.user?._id, "$likesDetails.likedBy"]},
+                        then: true,
+                        else: false
+                    }
+                }
+            }
+        },
+        {
+            $sort:{createdAt: -1}
+        },
+        {
+            $project:{
+                content: 1,
+                createdAt: 1,
+                likesCount: 1,
+                OwnerDetails: 1,
+                isLiked: 1
+            }
+        }
+    ]);
 
+    if (!tweet) {
+        throw new ApiError(500, "Something went wrong");
+    }
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, "Tweets fetched successfully", tweet));
 })
 
 const updateTweet = asyncHandler(async (req, res) => {
@@ -68,7 +142,7 @@ const updateTweet = asyncHandler(async (req, res) => {
         throw new ApiError(500, "Something went wrong");
     }
 
-    res.status(200).json(
+    return res.status(200).json(
         new ApiResponse(200, "Tweet updated successfully", updatedTweet)
     );
 });
@@ -95,7 +169,7 @@ const deleteTweet = asyncHandler(async (req, res) => {
     if (!deletedTweet) {
         throw new ApiError(500, "Something went wrong");
     }
-    res.status(200).json(new ApiResponse(200, "Tweet deleted successfully", deletedTweet));
+    return res.status(200).json(new ApiResponse(200, "Tweet deleted successfully", deletedTweet));
 
 });
 
