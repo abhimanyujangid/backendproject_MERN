f7f7b846ff7b66eaf621e403280389c4c905934c
abhimanyujangid <abhimanyujangid79@gmail.com>
2025-01-27 00:20:53 +0530

    Add video routes with CRUD operations and JWT verification for video management

--- src/app.js
+++ src/app.js
@@ -10,18 +10,9 @@ const app = express();
 const morganFormat = ":method :url :status :response-time ms";
 
 // Common middleware
-app.use(cors(
-    {
-        origin: process.env.CORS_ORIGIN,
-        credentials: true
-    }
-));
-app.use(express.json({
-    limit: '16kb'
-}));
-app.use(express.urlencoded({
-    extended: true
-}));
+app.use(cors({ origin:process.env.CORS_ORIGIN, credentials: true }));
+app.use(express.json({ limit: '16kb'}));
+app.use(express.urlencoded({ extended: true}));
 app.use(express.static('public'));
 
 // Logging middleware
@@ -48,6 +39,7 @@ import userRouter from './routes/user.routes.js';
 import tweetRouter from './routes/tweet.routes.js';
 import likeRouter from './routes/like.routes.js';
 import commentRouter from './routes/comment.routes.js';
+import videoRouter from './routes/video.routes.js';
 
 // routes
 app.use('/api/v1/healthChecked', healthCheckRouter);
@@ -55,6 +47,7 @@ app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tweets', tweetRouter);
 app.use('/api/v1/like', likeRouter);
 app.use("/api/v1/comment", commentRouter);
+app.use('/api/v1/video', videoRouter);
 
 // Error handling middleware
 // app.use(errorHandler);
