bc24817300681d6628810281744dfb9854fa414a
abhimanyujangid <abhimanyujangid79@gmail.com>
2025-01-29 18:54:10 +0530

    Add channel stats and videos endpoints with aggregation for subscribers, likes, and views

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -7,14 +7,26 @@ import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
 const getChannelStats = asyncHandler(async (req, res) => {
-    // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
-})
 
-const getChannelVideos = asyncHandler(async (req, res) => {
+    const userId = req.user?._id;
 
-    const userId = req.user?._id
-
-    const videos = await Video.aggregate([
+    const totalSubscribers = await Subscription.aggregate([
+        {
+            $match: {
+                channel: new mongoose.Types.ObjectId(userId)
+            }
+        },
+        {
+            $group: {
+                _id: null,
+                subscribersCount: {
+                    $sum: 1
+                }
+            }
+        }
+    ]);
+    
+    const video = await Video.aggregate([
         {
             $match: {
                 owner: new mongoose.Types.ObjectId(userId)
@@ -29,7 +41,65 @@ const getChannelVideos = asyncHandler(async (req, res) => {
             }
         },
         {
-            $count: "videoCount"
+            $project: {
+                totalLikes: {
+                    $size: "$likes"
+                },
+                totalViews: "$views",
+                totalVideos: 1
+            }
+        },
+        {
+            $group: {
+                _id: null,
+                totalLikes: {
+                    $sum: "$totalLikes"
+                },
+                totalViews: {
+                    $sum: "$totalViews"
+                },
+                totalVideos: {
+                    $sum: 1
+                }
+            }
+        }
+    ]);
+
+    const channelStats = {
+        totalSubscribers: totalSubscribers[0]?.subscribersCount || 0,
+        totalLikes: video[0]?.totalLikes || 0,
+        totalViews: video[0]?.totalViews || 0,
+        totalVideos: video[0]?.totalVideos || 0
+    };
+
+    return res.status(200).json(new ApiResponse(200, channelStats, "Channel stats retrieved successfully"));
+})
+
+const getChannelVideos = asyncHandler(async (req, res) => {
+
+    const userId = req.user?._id
+
+    const videos = await Video.aggregate([
+        {
+            $match: {
+                owner: new mongoose.Types.ObjectId(userId)
+            }
+        },
+        {
+            $group: {
+                _id: null,
+                totalVideo:{
+                    $sum: 1
+                }
+            }
+        },
+        {
+            $lookup: {
+                from: "likes",
+                localField: "_id",
+                foreignField: "video",
+                as: "likes"
+            }
         },
         {
           $addFields: {
@@ -41,9 +111,6 @@ const getChannelVideos = asyncHandler(async (req, res) => {
             likesCount: {
               $size: "$likes"
             },
-            videoCount:{
-                $size: "$_id"
-            }
           }
         },
         {
@@ -64,7 +131,8 @@ const getChannelVideos = asyncHandler(async (req, res) => {
                     month: 1,
                     day: 1
                 },
-                likesCount: 1                
+                likesCount: 1,
+                totalVideo: 1         
             }
         }
     ])
