b75318358c974ae0a8f743ca62546f86d4a856ba
abhimanyujangid <abhimanyujangid79@gmail.com>
2025-01-29 18:55:11 +0530

    Add dashboard routes integration in app.js

--- src/app.js
+++ src/app.js
@@ -41,6 +41,7 @@ import likeRouter from './routes/like.routes.js';
 import commentRouter from './routes/comment.routes.js';
 import videoRouter from './routes/video.routes.js';
 import playlistRouter from './routes/playlist.routes.js';
+import dashboardRouter from './routes/dashboard.routes.js';
 
 // routes
 app.use('/api/v1/healthChecked', healthCheckRouter);
@@ -50,6 +51,7 @@ app.use('/api/v1/like', likeRouter);
 app.use("/api/v1/comment", commentRouter);
 app.use('/api/v1/video', videoRouter);
 app.use('/api/v1/playlist', playlistRouter);
+app.use('/api/v1/dashboard', dashboardRouter);
 
 // Error handling middleware
 // app.use(errorHandler);
